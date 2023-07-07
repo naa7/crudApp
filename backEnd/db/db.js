041fb10ffcaaf4a54b0fab7eee794b7164a53ed0
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-06 23:47:22 -0400

    updated

--- backEnd/db/db.js
+++ backEnd/db/db.js
@@ -5,11 +5,14 @@ const { name } = require("../package.json");
 
 const { Pool } = require("pg");
 
-const db = new Sequelize(`postgres://naa7/crud-app:5432/${name}`, {
-  // username: process.env.USERNAME,
-  // password: process.env.PASSWORD,
-  logging: false,
-  dialect: require("pg"),
-});
+const db = new Sequelize(
+  `postgres://crud-app-back-end-omega.vercel.app:5432/${name}`,
+  {
+    // username: process.env.USERNAME,
+    // password: process.env.PASSWORD,
+    logging: false,
+    dialect: require("pg"),
+  }
+);
 
 module.exports = db;
