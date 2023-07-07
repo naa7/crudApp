7c7740b9ee9d45f58e35ae0c779c3959e4f0c052
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-07 00:16:52 -0400

    updated

--- backEnd/db/db.js
+++ backEnd/db/db.js
@@ -6,8 +6,6 @@ const { name } = require("../package.json");
 const { Pool } = require("pg");
 
 const db = new Sequelize(`${process.env.POSTGRES_URL}?sslmode=require`, {
-  // username: process.env.USERNAME,
-  // password: process.env.PASSWORD,
   logging: false,
   dialect: require("pg"),
 });
